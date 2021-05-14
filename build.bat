35c8c05ac6f5aca5521cfbe0a20ec01b77bade2b
Triskelia <triskelasmr@gmail.com>
2021-05-14 03:21:03 +0200

    Add 1.0 package

---
+++ build.bat
@@ -0,0 +1,39 @@
+@echo ====================================================================================
+@echo Prerequisite: Need to have the 7Zip directory added to the PATH environment variable
+@echo ====================================================================================
+@echo.
+
+@echo off
+@set /p version="version: "
+@set packageName=Bambi_Dumbtalk_Script_v%version%
+
+@cd %~dp0
+@set "PROJECT_DIR=%cd%"
+@cd "%PROJECT_DIR%"
+
+:: Reset the Build folder
+if exist "Build\" @rd /S /Q "Build"
+if not exist "Build\" @mkdir "Build"
+
+:: Create the package folder
+@mkdir "Build\%packageName%"
+
+:: Copy the necessary files to the package folder
+@robocopy "%PROJECT_DIR%" "Build\%packageName%" "README.md"
+@robocopy "%PROJECT_DIR%" "Build\%packageName%" "Bambi Dumbtalk Script.exe"
+@robocopy "%PROJECT_DIR%" "Build\%packageName%" "settings.ini"
+
+:: Zip the package
+@7z a -tzip "Build\%packageName%.zip" -r "%PROJECT_DIR%\Build\*"
+
+:: Delete the package folder to only leave the archive
+@rd /S /Q "Build\%packageName%"
+
+@echo.
+@echo ====================================================================================
+@echo Build Finished
+@echo ====================================================================================
+@echo Package location : "Build\%packageName%.zip"
+@echo.
+@echo.
+pause
